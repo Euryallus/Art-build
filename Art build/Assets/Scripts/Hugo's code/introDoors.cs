59f03077b820d7b90e6924a6aabdb5c5af264b5e
Hugo Bailey <lauren.bailey3@mail.bcu.ac.uk>
2020-08-23 11:20:19 +0100

    slope doors

---
+++ Art build/Assets/Scripts/Hugo's code/introDoors.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class introDoors : MonoBehaviour
+{
+    public GameObject doors;
+
+    private void Start()
+    {
+        doors.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        doors.SetActive(true);
+    }
+}
